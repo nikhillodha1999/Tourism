f0b8eb2db0800d3216495a750127e3cd62295774
nikhillodha1999 <118475684+nikhillodha1999@users.noreply.github.com>
2023-10-10 12:55:01 +0530

    .

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+
+	// Mobile menu
+	$('.mobile-menu-icon').click(function () {
+		$('.tm-nav').toggleClass('show');
+	});
+
+	$('body').bind('touchstart', function () { });
+});
